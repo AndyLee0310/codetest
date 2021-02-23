b568eb6b257de6dc10ba88c356d68456999db437
AndyLee <71858191+AndyLee0310@users.noreply.github.com>
2021-02-23 14:35:50 +0800

    update phototest

---
+++ PhotoTest/PhotoTest/PhotoTest/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace PhotoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label iDLabel;
+            System.Windows.Forms.Label photoLabel;
+            this.database1DataSet = new PhotoTest.Database1DataSet();
+            this.photoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.photoTableAdapter = new PhotoTest.Database1DataSetTableAdapters.photoTableAdapter();
+            this.tableAdapterManager = new PhotoTest.Database1DataSetTableAdapters.TableAdapterManager();
+            this.photoDataGridView = new System.Windows.Forms.DataGridView();
+            this.iDLabel1 = new System.Windows.Forms.Label();
+            this.photoPictureBox = new System.Windows.Forms.PictureBox();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.NewBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.DelBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.CelBtn = new System.Windows.Forms.Button();
+            iDLabel = new System.Windows.Forms.Label();
+            photoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // photoBindingSource
+            // 
+            this.photoBindingSource.DataMember = "photo";
+            this.photoBindingSource.DataSource = this.database1DataSet;
+            // 
+            // photoTableAdapter
+            // 
+            this.photoTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.photoTableAdapter = this.photoTableAdapter;
+            this.tableAdapterManager.UpdateOrder = PhotoTest.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // photoDataGridView
+            // 
+            this.photoDataGridView.AllowUserToAddRows = false;
+            this.photoDataGridView.AllowUserToDeleteRows = false;
+            this.photoDataGridView.AutoGenerateColumns = false;
+            this.photoDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.photoDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.photoDataGridView.DataSource = this.photoBindingSource;
+            this.photoDataGridView.Location = new System.Drawing.Point(11, 8);
+            this.photoDataGridView.Name = "photoDataGridView";
+            this.photoDataGridView.ReadOnly = true;
+            this.photoDataGridView.RowTemplate.Height = 24;
+            this.photoDataGridView.Size = new System.Drawing.Size(112, 310);
+            this.photoDataGridView.TabIndex = 1;
+            // 
+            // iDLabel
+            // 
+            iDLabel.AutoSize = true;
+            iDLabel.Location = new System.Drawing.Point(17, 8);
+            iDLabel.Name = "iDLabel";
+            iDLabel.Size = new System.Drawing.Size(20, 12);
+            iDLabel.TabIndex = 1;
+            iDLabel.Text = "ID:";
+            // 
+            // iDLabel1
+            // 
+            this.iDLabel1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.photoBindingSource, "ID", true));
+            this.iDLabel1.Location = new System.Drawing.Point(58, 8);
+            this.iDLabel1.Name = "iDLabel1";
+            this.iDLabel1.Size = new System.Drawing.Size(100, 23);
+            this.iDLabel1.TabIndex = 2;
+            this.iDLabel1.Text = "label1";
+            // 
+            // photoLabel
+            // 
+            photoLabel.AutoSize = true;
+            photoLabel.Location = new System.Drawing.Point(17, 34);
+            photoLabel.Name = "photoLabel";
+            photoLabel.Size = new System.Drawing.Size(35, 12);
+            photoLabel.TabIndex = 3;
+            photoLabel.Text = "Photo:";
+            // 
+            // photoPictureBox
+            // 
+            this.photoPictureBox.DataBindings.Add(new System.Windows.Forms.Binding("Image", this.photoBindingSource, "Photo", true));
+            this.photoPictureBox.Location = new System.Drawing.Point(58, 34);
+            this.photoPictureBox.Name = "photoPictureBox";
+            this.photoPictureBox.Size = new System.Drawing.Size(220, 243);
+            this.photoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.photoPictureBox.TabIndex = 4;
+            this.photoPictureBox.TabStop = false;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(58, 288);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(220, 24);
+            this.LoadBtn.TabIndex = 5;
+            this.LoadBtn.Text = "Load...";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LoadBtn);
+            this.panel1.Controls.Add(iDLabel);
+            this.panel1.Controls.Add(this.iDLabel1);
+            this.panel1.Controls.Add(photoLabel);
+            this.panel1.Controls.Add(this.photoPictureBox);
+            this.panel1.Location = new System.Drawing.Point(155, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 331);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.photoDataGridView);
+            this.panel2.Location = new System.Drawing.Point(12, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(137, 331);
+            this.panel2.TabIndex = 7;
+            // 
+            // NewBtn
+            // 
+            this.NewBtn.Location = new System.Drawing.Point(12, 15);
+            this.NewBtn.Name = "NewBtn";
+            this.NewBtn.Size = new System.Drawing.Size(74, 22);
+            this.NewBtn.TabIndex = 8;
+            this.NewBtn.Text = "New";
+            this.NewBtn.UseVisualStyleBackColor = true;
+            this.NewBtn.Click += new System.EventHandler(this.NewBtn_Click);
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Location = new System.Drawing.Point(92, 15);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(74, 22);
+            this.EditBtn.TabIndex = 8;
+            this.EditBtn.Text = "Edit";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            this.EditBtn.Click += new System.EventHandler(this.EditBtn_Click);
+            // 
+            // DelBtn
+            // 
+            this.DelBtn.Location = new System.Drawing.Point(172, 15);
+            this.DelBtn.Name = "DelBtn";
+            this.DelBtn.Size = new System.Drawing.Size(74, 22);
+            this.DelBtn.TabIndex = 8;
+            this.DelBtn.Text = "Delete";
+            this.DelBtn.UseVisualStyleBackColor = true;
+            this.DelBtn.Click += new System.EventHandler(this.DelBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(252, 15);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(74, 22);
+            this.SaveBtn.TabIndex = 8;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // CelBtn
+            // 
+            this.CelBtn.Location = new System.Drawing.Point(332, 15);
+            this.CelBtn.Name = "CelBtn";
+            this.CelBtn.Size = new System.Drawing.Size(74, 22);
+            this.CelBtn.TabIndex = 8;
+            this.CelBtn.Text = "Cancel";
+            this.CelBtn.UseVisualStyleBackColor = true;
+            this.CelBtn.Click += new System.EventHandler(this.CelBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 395);
+            this.Controls.Add(this.CelBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.DelBtn);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.NewBtn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "PhotoTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource photoBindingSource;
+        private Database1DataSetTableAdapters.photoTableAdapter photoTableAdapter;
+        private Database1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView photoDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.Label iDLabel1;
+        private System.Windows.Forms.PictureBox photoPictureBox;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button NewBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.Button DelBtn;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button CelBtn;
+    }
+}
+
---
+++ PhotoTest/PhotoTest/PhotoTest/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+using System.IO;
+
+namespace PhotoTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void photoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.photoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.photo' table. You can move, or remove it, as needed.
+            this.photoTableAdapter.Fill(this.database1DataSet.photo);
+            Read(null, null);
+        }
+
+        private void NewBtn_Click(object sender, EventArgs e)
+        {
+            Write(null, null);
+            this.photoBindingSource.AddNew();
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            Write(null, null);
+        }
+
+        private void DelBtn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to delete this data?", "Question", MessageBoxButtons.YesNo) == DialogResult.Yes ){
+                this.photoBindingSource.RemoveCurrent ();
+                SaveBtn_Click(sender, e);
+            }
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if (photoPictureBox.Image is null)
+            {
+                MessageBox.Show("Photo File is not empty!!", "Note");
+            }else{
+                photoBindingNavigatorSaveItem_Click(sender, e);
+                this.photoTableAdapter.Fill(this.database1DataSet.photo);
+                Read(null, null);
+            }
+        }
+
+        private void CelBtn_Click(object sender, EventArgs e)
+        {
+            this.photoTableAdapter.Fill(this.database1DataSet.photo);
+            Read(null, null);
+        }
+
+        string imgname;
+        OleDbDataAdapter diaimage;
+        DataSet dsimage;
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileDialog dlgimage = new OpenFileDialog();
+                dlgimage.Filter = "Image file (*.jpg;*.png)|*.jpg;*.png| All file (*.*)|*.*";
+                if(dlgimage.ShowDialog() == DialogResult.OK)
+                {
+                    imgname = dlgimage.FileName;
+                    Bitmap newimg = new Bitmap(imgname);
+                    photoPictureBox.Image = (Image)newimg; //directcast(newimg, Image);
+                }
+                dlgimage = null;
+            }
+            catch (ArgumentException ae){
+                imgname = " ";
+                MessageBox.Show(ae.Message .ToString());
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void Write(object sender, EventArgs e)
+        {
+            NewBtn.Enabled = false;
+            EditBtn.Enabled = false;
+            DelBtn.Enabled = false;
+            SaveBtn.Enabled = true;
+            CelBtn.Enabled = true;
+            panel1.Enabled = true;
+            panel2.Enabled = false;
+
+        }
+        private void Read(object sender, EventArgs e)
+        {
+            NewBtn.Enabled = true;
+            EditBtn.Enabled = true;
+            DelBtn.Enabled = true;
+            SaveBtn.Enabled = false;
+            CelBtn.Enabled = false;
+            panel1.Enabled = false;
+            panel2.Enabled = true;
+
+        }
+    }
+}
